f38f4eb48b71661a937afdab0fa2a6c5e7fb638c
Tasnin <tasnintithi@gmail.com>
2021-11-27 20:58:11 +0600

    second-commit

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -9,7 +9,7 @@ const AddService = () => {
     const onSubmit = data => {
         console.log(data);
 
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://sheltered-brook-96434.herokuapp.com/services', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('Added Successfully');
